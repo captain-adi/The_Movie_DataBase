415d7820be2faf7e87b02245e4beed17f25d0560
captain-adi <maniramp554@gmail.com>
2025-12-27 11:34:05 +0530

    feat : setup react router along with hero page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
-import Welcome from "./components/welcome/Welcome";
 
 function App() {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
       <main className="flex-1">
-        <Welcome />
+        <Outlet />
       </main>
       <Footer />
     </div>
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,11 @@
+import Welcome from "../welcome/Welcome";
+
+function Hero() {
+  return (
+    <section>
+      <Welcome />
+    </section>
+  );
+}
+
+export default Hero;
