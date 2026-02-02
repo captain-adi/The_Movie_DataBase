775966dccda49ed2964a7bb093c9f07b091b17d8
captain-adi <maniramp554@gmail.com>
2026-02-02 11:53:34 +0530

    feat: add routes for CastNCrew and PersonDetails components

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,8 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "@/App";
 import Detailedpage from "@/pages/detailedpage/Detailedpage";
 import Hero from "@/pages/Hero/Hero";
+import CastNCrew from "@/pages/castNcrew/CastNCrew";
+import PersonDetails from "@/pages/personDetailes/PersonDetails";
 
 const router = createBrowserRouter([
   {
@@ -16,6 +18,14 @@ const router = createBrowserRouter([
       {
         path: "/details/:type/:id",
         element: <Detailedpage />,
+      },
+      {
+        path: "/cast&crew/:type/:id/cast",
+        element: <CastNCrew />,
+      },
+      {
+        path: "/person/:id",
+        element: <PersonDetails />,
       },
     ],
   },
